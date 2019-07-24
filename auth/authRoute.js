c1df014ec5eab7899769df3bbd6706649c8964c4
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-24 15:13:25 +0100

    generate user token

--- auth/authRoute.js
+++ auth/authRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const { createUser } = require('../users/usersModel');
+const { createUser, findUsername } = require('../users/usersModel');
 const validateUserMiddleware = require('./validateUserMiddleware');
-
+const generateToken = require('./generateToken');
 const router = express.Router();
 
 router.post('/register', validateUserMiddleware, async (req, res) => {
@@ -14,14 +14,33 @@ router.post('/register', validateUserMiddleware, async (req, res) => {
       password: hashedPassword,
       department
     });
-      return res.status(201).json({
-          message: 'user created successfully',
-          user
-      });
+    return res.status(201).json({
+      message: 'user created successfully',
+      user
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ error: 'could not create user' });
   }
 });
+
+router.post('/login', async (req, res) => {
+    try {
+      let { username, password } = req.body;
+      const user = await findUsername(username).first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+          const token = generateToken(user);
+          console.log(user)
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'You shall not pass' });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
 
 module.exports = router;
